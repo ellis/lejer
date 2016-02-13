168a8e3dbb0fb30f2c9723dcd3f21fb50e92d0a2
Ellis Whitehead <ellis@ellisw.net>
2016-02-13 07:24:43 +0100

    relic spotter

--- src/lib/consoleTable.js
+++ src/lib/consoleTable.js
@@ -7,23 +7,37 @@ export function getTableString(rows, columnNames) {
 		widthCols[i] = Math.max(_.get(widthCols, i, 0), s.toString().length);
 	});
 	// Widths of columns in rows
-	_.forEach(rows, row => {
-		_.forEach(row, (s, i) => {
-			if (!_.isUndefined(s)) {
-				widthCols[i] = Math.max(_.get(widthCols, i, 0), s.toString().length);
-			}
+	function updateWidths(rows) {
+		_.forEach(rows, row => {
+			_.forEach(row, (s, i) => {
+				if (!_.isUndefined(s)) {
+					widthCols[i] = Math.max(_.get(widthCols, i, 0), s.toString().length);
+				}
+			});
 		});
-	});
+	}
+	updateWidths([columnNames]);
+	updateWidths(rows);
 	console.log({widthCols});
 
 	const lines = [];
-	_.forEach(rows, row => {
-		const line = _.map(row, (s, i) => (
-			_.padEnd(s, widthCols[i])
-		)).join("  ");
-		lines.push(line);
-		//console.log(_.padEnd(indentText+account, widthCol1) + "    " + balance);
-	});
-	//console.log();
+	function addLines(rows) {
+		_.forEach(rows, row => {
+			const line = _.map(row, (s, i) => (
+				_.padEnd(s, widthCols[i])
+			)).join("  ");
+			lines.push(line);
+			//console.log(_.padEnd(indentText+account, widthCol1) + "    " + balance);
+		});
+	}
+	if (!_.isEmpty(columnNames)) {
+		// Column names
+		addLines([columnNames]);
+		// Separator
+		lines.push(_.map(widthCols, n => _.repeat("=", n)).join("  "));
+	}
+	// Row values
+	addLines(rows);
+
 	return lines.join("\n");
 }
--- src/relic_spotter.js
+++ src/relic_spotter.js
@@ -1,9 +1,13 @@
+/**
+ * See https://www.coursera.org/learn/wharton-accounting/lecture/3MXca/3-1-2-relic-spotter-case-part-5
+ */
 import _ from 'lodash';
 import {getTableString} from './lib/consoleTable.js';
 
 const transactions = [
 	{
 		id: 1,
+		description: "Sell shares",
 		accounts: {
 			"assets:cash": [{amount: 250000, bucket: "financing"}],
 			"equity:common stock": [{amount: -25000}],
@@ -12,13 +16,41 @@ const transactions = [
 	},
 	{
 		id: 3,
+		description: "Legal fees",
 		accounts: {
 			"assets:cash": [{amount: -3900, bucket: "operating"}],
 			"expenses:legal fees": [{amount: 3900}]
 		}
 	},
 	{
+		id: 4,
+		description: "Buy building and land",
+		accounts: {
+			"assets:cash": [{amount: 124000, bucket: "financing"}, {amount: -155000, bucket: "investing"}],
+			"liabilities:mortgage payable": [{amount: -124000}],
+			"assets:building": [{amount: 52000}],
+			"assets:land": [{acount: 103000}]
+		}
+	},
+	{
+		id: 5,
+		description: "Building renovation",
+		accounts: {
+			"assets:cash": [{amount: -33000, bucket: "investing"}],
+			"assets:building": [{amount: 33000}],
+		}
+	},
+	{
+		id: 6,
+		description: "Buy metal detectors",
+		accounts: {
+			"assets:cash": [{amount: -120000, bucket: "investing"}],
+			"assets:metal detectors": [{amount: 120000}],
+		}
+	},
+	{
 		id: 3,
+		description: "",
 		accounts: {
 			"assets:cash": [{amount: 0}],
 			"": [{amount: 0}],
@@ -27,6 +59,7 @@ const transactions = [
 	},
 	{
 		id: 3,
+		description: "",
 		accounts: {
 			"assets:cash": [{amount: 0}],
 			"": [{amount: 0}],
@@ -35,6 +68,7 @@ const transactions = [
 	},
 	{
 		id: 3,
+		description: "",
 		accounts: {
 			"assets:cash": [{amount: 0}],
 			"": [{amount: 0}],
@@ -43,6 +77,34 @@ const transactions = [
 	},
 	{
 		id: 3,
+		description: "",
+		accounts: {
+			"assets:cash": [{amount: 0}],
+			"": [{amount: 0}],
+			"": [{amount: 0}],
+		}
+	},
+	{
+		id: 3,
+		description: "",
+		accounts: {
+			"assets:cash": [{amount: 0}],
+			"": [{amount: 0}],
+			"": [{amount: 0}],
+		}
+	},
+	{
+		id: 3,
+		description: "",
+		accounts: {
+			"assets:cash": [{amount: 0}],
+			"": [{amount: 0}],
+			"": [{amount: 0}],
+		}
+	},
+	{
+		id: 3,
+		description: "",
 		accounts: {
 			"assets:cash": [{amount: 0}],
 			"": [{amount: 0}],
@@ -64,7 +126,7 @@ function report1() {
 			_.forEach(accountEntries, accountEntry => {
 				const bucketCol = _.get(bucketToCol, accountEntry.bucket);
 				if (bucketCol > 0) {
-					const row = [t.id.toString(), accountName, accountEntry.amount.toString(), "", "", ""];
+					const row = [t.id.toString(), t.description || accountName, accountEntry.amount.toString(), "", "", ""];
 					row[bucketCol] = accountEntry.amount.toString();
 					rows.push(row);
 				}
@@ -72,7 +134,7 @@ function report1() {
 		});
 	});
 
-	console.log(getTableString(rows));
+	console.log(getTableString(rows, ["No.", "Transaction", "Cash", "Operating", "Investing", "Financing"]));
 }
 
 report1();
