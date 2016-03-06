aa587fbc2e2e9f1871960275192128ae3e3b8b38
Ellis Whitehead <ellis@ellisw.net>
2016-03-06 19:42:22 +0100

    PASSING

--- src/server/src/core2.js
+++ src/server/src/core2.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { List, Map, fromJS } from 'immutable';
+import moment from 'moment';
 
 export const INITIAL_STATE = Map();
 
@@ -31,10 +32,34 @@ export function mergeTransaction(state, basename, entryId, t) {
 			const sumInOut = (amount < 0) ? "sumOut" : "sumIn";
 			state = state.updateIn(["accountEntries", accountName, phase, sumInOut], 0, n => n + amount);
 
-			// Add cash transaction to cashTransactionEntries
+			/*// Add cash transaction to cashTransactionEntries
 			const cashActivity = _.get(accountEntry, ["tags", "reports/cash/activity"]);
 			if (cashActivity) {
 				state = state.updateIn(["reports", "cash"], List(), l => {
+					return l.push(fromJS(_.merge({}, {date: t.date, id: t.id, cash: amount, [cashActivity]: amount})));
+				});
+			}*/
+		});
+	});
+
+	state = addTransactionToReportCash(state, t);
+
+	return state;
+}
+
+function addTransactionToReportCash(state, t) {
+	const date0 = _.get(t, ["tags", "report/cash/date"], t.date);
+
+	_.forEach(t.accounts, (accountEntries, accountName) => {
+		_.forEach(accountEntries, accountEntry => {
+			const amount = accountEntry.amount || 0;
+
+			const cashActivity = _.get(accountEntry, ["tags", "reports/cash/activity"]);
+			const date = _.get(accountEntry, ["tags", "report/cash/date"], date0);
+			// Add cash transaction to cashTransactionEntries
+			if (cashActivity && date) {
+				const period = moment(date).year().toString();
+				state = state.updateIn(["reports", "cash", period], List(), l => {
 					return l.push(fromJS(_.merge({}, {date: t.date, id: t.id, cash: amount, [cashActivity]: amount})));
 				});
 			}
--- src/server/test/coreTest.js
+++ src/server/test/coreTest.js
@@ -18,7 +18,7 @@ const t03 = {
 	data: "2012-04-02",
 	description: "Legal fees",
 	accounts: {
-		"assets:current:cash": [{amount: -3900, tags: {"reports/cash/activity": "operating"}}}],
+		"assets:current:cash": [{amount: -3900, tags: {"reports/cash/activity": "operating"}}],
 		"expenses:period:legal fees": [{amount: 3900}]
 	}
 };
@@ -37,27 +37,18 @@ describe.only('core logic', () => {
 
 	describe('mergeTransaction', () => {
 
-		it('merges the transaction into state', () => {
+		it('merges a transaction into state', () => {
 			const state0 = Map();
 			const state = mergeTransaction(state0, "RelicSpotter", 1, t01);
 			console.log(JSON.stringify(state, null, '\t'))
 			expect(state).to.equal(fromJS({
 				"transactions": {
 					"RelicSpotter": {
-						"1": {
-							"id": "01",
-							"date": "2012-04-01",
-							"description": "Sell shares",
-							"accounts": {
-								"assets:cash": [ { "amount": 250000, "tags": { "reports/cash/activity": "financing" } } ],
-								"equity:common stock": [ { "amount": -25000 } ],
-								"equity:additional paid-in capital": [ { "amount": -225000 } ]
-							}
-						}
+						"1": t01
 					}
 				},
 				"accountEntries": {
-					"assets:cash": {
+					"assets:current:cash": {
 						"unadjusted": { "entries": { "1": 250000 }, "sum": 250000, "sumIn": 250000 }
 					},
 					"equity:common stock": {
@@ -68,9 +59,11 @@ describe.only('core logic', () => {
 					}
 				},
 				"reports": {
-					"cash": [
-						{ "date": "2012-04-01", "cash": 250000, "financing": 250000 }
-					]
+					"cash": {
+						"2012": [
+							{ "id": "01", "date": "2012-04-01", "cash": 250000, "financing": 250000 }
+						]
+					}
 				}
 			}));
 		});
