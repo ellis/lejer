872fd91120c56697a7f61619687785cf95e4b2f8
Ellis Whitehead <ellis@ellisw.net>
2016-04-03 17:53:13 +0200

    Update core functions to use Amount and fix a deprecation warning for moment.js

--- src/server/src/core2.js
+++ src/server/src/core2.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import { List, Map, fromJS } from 'immutable';
 import naturalSort from 'javascript-natural-sort';
 import moment from 'moment';
+import * as Amount from './amount.js';
 
 export const INITIAL_STATE = Map();
 
@@ -28,10 +29,10 @@ export function mergeTransaction(state, basename, entryId, t) {
 			const amount = accountEntry.amount || 0;
 
 			// Add transaction to accountEntries
-			state = state.updateIn(["accountEntries", accountName, phase, "entries", entryId], 0, n => n + amount);
-			state = state.updateIn(["accountEntries", accountName, phase, "sum"], 0, n => n + amount);
+			state = state.updateIn(["accountEntries", accountName, phase, "entries", entryId], 0, n => Amount.add(n, amount));
+			state = state.updateIn(["accountEntries", accountName, phase, "sum"], 0, n => Amount.add(n, amount));
 			const sumInOut = (amount < 0) ? "sumOut" : "sumIn";
-			state = state.updateIn(["accountEntries", accountName, phase, sumInOut], 0, n => n + amount);
+			state = state.updateIn(["accountEntries", accountName, phase, sumInOut], 0, n => Amount.add(n, amount));
 
 			/*// Add cash transaction to cashTransactionEntries
 			const cashActivity = _.get(accountEntry, ["tags", "reports/cash/activity"]);
@@ -63,7 +64,7 @@ function addTransactionToReportCash(state, t) {
 			const date = _.get(accountEntry, ["tags", "report/cash/date"], date0);
 			// Add cash transaction to cashTransactionEntries
 			if (cashActivity && date) {
-				const period = moment(date).year().toString();
+				const period = moment(date, "YYYY-MM-DD").year().toString();
 				state = state.updateIn(["reports", "cash", period], List(), l => {
 					return l.push(fromJS(_.merge({}, {date: t.date, id: t.id, cash: amount, [cashActivity]: amount})));
 				});
@@ -87,45 +88,45 @@ function addTransactionToReportIncome(state, t) {
 				return;
 			}
 
-			const period = moment(date).year().toString();
+			const period = moment(date, "YYYY-MM-DD").year().toString();
 			let report = state.getIn(["reports", "income", period], Map());
 			const accountPath = accountName.split(":");
 			// console.log({accountPath})
 			if (_.isEqual(["revenues", "primary"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["revenues", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["revenues", "total"], 0, n => n + -amount);
+					.updateIn(["revenues", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["revenues", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["expenses", "primary"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["costOfRevenues", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["costOfRevenues", "total"], 0, n => n + -amount);
+					.updateIn(["costOfRevenues", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["costOfRevenues", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["expenses", "period"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["periodExpenses", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["periodExpenses", "total"], 0, n => n + -amount);
+					.updateIn(["periodExpenses", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["periodExpenses", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["revenues", "secondary"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["secondaryGains", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["secondaryGains", "total"], 0, n => n + -amount);
+					.updateIn(["secondaryGains", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["secondaryGains", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["expenses", "secondary"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["secondaryLosses", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["secondaryLosses", "total"], 0, n => n + -amount);
+					.updateIn(["secondaryLosses", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["secondaryLosses", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["expenses", "income tax"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["incomeTax", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["incomeTax", "total"], 0, n => n + -amount);
+					.updateIn(["incomeTax", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["incomeTax", "total"], 0, n => Amount.subtract(n, amount));
 			}
 
 			state = state.setIn(["reports", "income", period], report);
@@ -148,7 +149,7 @@ function addTransactionToReportBalance(state, t) {
 				return;
 			}
 
-			const period = moment(date).year().toString();
+			const period = moment(date, "YYYY-MM-DD").year().toString();
 			let report = state.getIn(["reports", "balance", period], Map());
 			const accountPath = accountName.split(":");
 			// console.log({accountPath})
@@ -157,50 +158,50 @@ function addTransactionToReportBalance(state, t) {
 			if (_.isEqual(["assets", "current"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["assetsCurrent", "accounts", accountName2], 0, n => n + amount)
-					.updateIn(["assetsCurrent", "total"], 0, n => n + amount)
-					.updateIn(["debitsTotal", "total"], 0, n => n + amount);
+					.updateIn(["assetsCurrent", "accounts", accountName2], 0, n => Amount.add(n, amount))
+					.updateIn(["assetsCurrent", "total"], 0, n => Amount.add(n, amount))
+					.updateIn(["debitsTotal", "total"], 0, n => Amount.add(n, amount));
 			}
 			else if (_.isEqual(["assets", "long-term"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["assetsLongterm", "accounts", accountName2], 0, n => n + amount)
-					.updateIn(["assetsLongterm", "total"], 0, n => n + amount)
-					.updateIn(["debitsTotal", "total"], 0, n => n + amount);
+					.updateIn(["assetsLongterm", "accounts", accountName2], 0, n => Amount.add(n, amount))
+					.updateIn(["assetsLongterm", "total"], 0, n => Amount.add(n, amount))
+					.updateIn(["debitsTotal", "total"], 0, n => Amount.add(n, amount));
 			}
 			else if (_.isEqual(["assets", "intangible"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["assetsIntangible", "accounts", accountName2], 0, n => n + amount)
-					.updateIn(["assetsIntangible", "total"], 0, n => n + amount)
-					.updateIn(["debitsTotal", "total"], 0, n => n + amount);
+					.updateIn(["assetsIntangible", "accounts", accountName2], 0, n => Amount.add(n, amount))
+					.updateIn(["assetsIntangible", "total"], 0, n => Amount.add(n, amount))
+					.updateIn(["debitsTotal", "total"], 0, n => Amount.add(n, amount));
 			}
 
 			// Liabilities
 			else if (_.isEqual(["liabilities", "current"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["liabilitiesCurrent", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["liabilitiesCurrent", "total"], 0, n => n + -amount)
-					.updateIn(["liabilitiesTotal", "total"], 0, n => n + -amount)
-					.updateIn(["creditsTotal", "total"], 0, n => n + -amount);
+					.updateIn(["liabilitiesCurrent", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["liabilitiesCurrent", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["liabilitiesTotal", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["creditsTotal", "total"], 0, n => Amount.subtract(n, amount));
 			}
 			else if (_.isEqual(["liabilities", "long-term"], _.take(accountPath, 2))) {
 				const accountName2 = _.drop(accountPath, 2).join(":") || "general";
 				report = report
-					.updateIn(["liabilitiesLongterm", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["liabilitiesLongterm", "total"], 0, n => n + -amount)
-					.updateIn(["liabilitiesTotal", "total"], 0, n => n + -amount)
-					.updateIn(["creditsTotal", "total"], 0, n => n + -amount);
+					.updateIn(["liabilitiesLongterm", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["liabilitiesLongterm", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["liabilitiesTotal", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["creditsTotal", "total"], 0, n => Amount.subtract(n, amount));
 			}
 
 			// Liabilities
 			else if (_.isEqual(["equity"], _.take(accountPath, 1))) {
 				const accountName2 = _.drop(accountPath, 1).join(":") || "general";
 				report = report
-					.updateIn(["equity", "accounts", accountName2], 0, n => n + -amount)
-					.updateIn(["equity", "total"], 0, n => n + -amount)
-					.updateIn(["creditsTotal", "total"], 0, n => n + -amount);
+					.updateIn(["equity", "accounts", accountName2], 0, n => Amount.subtract(n, amount))
+					.updateIn(["equity", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["creditsTotal", "total"], 0, n => Amount.subtract(n, amount));
 			}
 
 			state = state.setIn(["reports", "balance", period], report);
@@ -221,7 +222,7 @@ function addTransactionToReportCashflow(state, t) {
 			const date = _.get(accountEntry, ["tags", "report/cash/date"], date0);
 			// Add cash transaction to cashTransactionEntries
 			if (cashActivity && date) {
-				const period = moment(date).year().toString();
+				const period = moment(date, "YYYY-MM-DD").year().toString();
 
 				state = state.updateIn(["reports", "cashflow", period, cashActivity], Map(), m => {
 					return m.updateIn(
@@ -254,11 +255,11 @@ function updateClosingTransactions(state, t) {
 				return;
 			}
 
-			const period = moment(date).year().toString();
+			const period = moment(date, "YYYY-MM-DD").year().toString();
 			let report = state.getIn(["reports", "balance", period], Map());
 			const accountPath = accountName.split(":");
 			// Date for end of period/year
-			const dateClosing = moment(`${period+1}-01-01`).subtract(1, 'day').format('YYYY-MM-DD');
+			const dateClosing = moment(`${period+1}-01-01`, "YYYY-MM-DD").subtract(1, 'day').format('YYYY-MM-DD');
 			// console.log({accountPath})
 
 			const isRevenue = _.isEqual(["revenues"], _.take(accountPath, 1));
@@ -298,9 +299,9 @@ function updateClosingTransactions(state, t) {
 			// Update equity balance
 			if (isRevenue || isExpense) {
 				report = report
-					.updateIn(["equity", "accounts", "retained earnings"], 0, n => n + -amount)
-					.updateIn(["equity", "total"], 0, n => n + -amount)
-					.updateIn(["creditsTotal", "total"], 0, n => n + -amount);
+					.updateIn(["equity", "accounts", "retained earnings"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["equity", "total"], 0, n => Amount.subtract(n, amount))
+					.updateIn(["creditsTotal", "total"], 0, n => Amount.subtract(n, amount));
 					state = state.setIn(["reports", "balance", period], report);
 			}
 		});
